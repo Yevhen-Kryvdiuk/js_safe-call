db353ae31f3a78acd5d0474fbe8470dacba1546e
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-05-30 18:24:53 +0300

    fixed

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// function safeCall(f) {
-//     try {
-//         f();
-//         console.log(true);
-//     } catch (error) {
-//         console.log(false);
-//     }
-// }
-//
-// safeCall(() => console.log('Hello!')); // true
-// safeCall(() => JSON.parse('abc')); // false
-// safeCall(() => false); // true
-// safeCall(() => abc); // false
+function safeCall(f) {
+    try {
+        f();
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+safeCall(() => console.log('Hello!')); // true
+safeCall(() => JSON.parse('abc')); // false
+safeCall(() => false); // true
+safeCall(() => abc); // false
 
 
 Function.prototype.safeCall = function (context, ...args) {
